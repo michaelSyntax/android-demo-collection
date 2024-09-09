e6f32c6e2bd1b0c2bbd8d8171543aec7713bf3f9
michaelSyntax <michael.karbe@syntax-institut.de>
2024-09-10 00:56:17 +0200

    set navigation ids

--- app/src/main/java/com/example/democollection/MainActivity.kt
+++ app/src/main/java/com/example/democollection/MainActivity.kt
@@ -42,7 +42,11 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_animations_motion_layout,
+                R.id.nav_animations_object_animator,
+                R.id.nav_animations_property,
+                R.id.nav_animations_transition_manager,
+                R.id.nav_animations_vector
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
